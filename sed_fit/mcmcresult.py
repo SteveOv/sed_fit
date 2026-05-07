214f1b51333e6c555003149684f7cd1db76a3395
SteveOv <steveoverall@hotmail.co.uk>
2026-05-07 11:43:28 +0100

    calirified naming or iters to samples (relates to #7)

--- sed_fit/mcmcresult.py
+++ sed_fit/mcmcresult.py
@@ -29,39 +29,39 @@ class McmcResult():
 
     @property
     def thin_by(self) -> int:
-        """ The number of steps the samples were thinned by. """
+        """ The number of steps at which each sample is taken. """
         return self._thin_by
 
     @property
-    def tau_iters(self) -> _np.ndarray:
+    def tau_samples(self) -> _np.ndarray:
         """
-        The autocorrelation (tau) iterations (steps/thin_by) for each fitted param.
-        These are the estimated number of iterations to 'forget' the start position.
+        The number of autocorrelation samples (steps/thin_by) for each fitted param.
+        These are the estimated number of samples to 'forget' the start position.
         """
         return self._sampler.get_autocorr_time(c=5, tol=self._autocorr_tol, quiet=True)
 
     @property
-    def burn_in_iters(self) -> int:
-        """ The estimated number of iterations (steps/thin_by) for the burn-in """
+    def burn_in_samples(self) -> int:
+        """ The estimated number of samples (steps/thin_by) for the burn-in """
         def_tau_iters = self._sampler.iteration / 25
-        return int(_np.ceil(max(_np.nan_to_num(self.tau_iters, copy=True, nan=def_tau_iters)) * 5))
+        return int(_np.ceil(max(_np.nan_to_num(self.tau_samples, copy=True, nan=def_tau_iters)) * 5))
 
     @property
     def burn_in_steps(self) -> int:
-        """ The estimated number of steps (iterations * thin_by) for the burn-in """
-        return self.burn_in_iters * self._thin_by
+        """ The estimated number of steps (samples * thin_by) for the burn-in """
+        return self.burn_in_samples * self._thin_by
 
     def get_sample_chain(self, flat: bool=False, discard: int=None) -> _np.ndarray:
         """
-        Get the chain of iteration samples, optionally without the burn-in iterations.
-        These samples will have been taken every iteration (1 iteration every thin_by step).
+        Get the chain of sampled parameter values, optionally after discarding the burn-in samples.
+        These samples will have been taken after every 'thin_by' step(s).
 
         :flat: whether or not to flatten the chain
-        :discard: number of "burn in" iters to omit from the chain, or burn_in_iters if None
-        :returns: the requested samples
+        :discard: number of "burn in" samples to omit from the chain, or burn_in_samples if None
+        :returns: the requested set of sampled parameter values
         """
         if discard is None:
-            discard = self.burn_in_iters
+            discard = self.burn_in_samples
         return self._sampler.get_chain(discard=discard, flat=flat)
 
     def get_theta(self,
@@ -78,7 +78,7 @@ class McmcResult():
         Fixed theta values will be given an uncertainty of zero.
 
         :quantiles: the quantiles over which to summarise the samples
-        :discard: number of "burn in" iters to omit from the samples, or burn_in_iters if None
+        :discard: number of "burn in" samples to omit from the summary, or burn_in_samples if None
         :returns: the final theta from the sample chain with +/- uncertainties for fitted values
         """
         if isinstance(quantiles, _Tuple) and 2 <= len(quantiles) <= 3:
@@ -96,5 +96,5 @@ class McmcResult():
 
     def __str__(self):
         return f"""Mean Acceptance fraction:    {_np.mean(self._sampler.acceptance_fraction):.3f}
-Autocorrelation steps (tau): {', '.join(f'{t:.3f}' for t in self.tau_iters * self._thin_by)}
+Autocorrelation steps (tau): {', '.join(f'{t:.3f}' for t in self.tau_samples * self._thin_by)}
 Estimated burn-in steps:     {self.burn_in_steps:,}"""
