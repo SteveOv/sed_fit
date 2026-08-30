7e77a8026d4b3cacadc6dbfd34ad7e0b9a18a61f
SteveOv <steveoverall@hotmail.co.uk>
2026-08-30 15:14:57 +0100

    refactored fit_testing so we can extend nstars in future (relates to #21)

--- fit_testing.py
+++ fit_testing.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Testing SED fitting against known targets """
 # pylint: disable=no-member, invalid-name, no-name-in-module
+from typing import List
 import warnings
 from pathlib import Path
 from shutil import rmtree
@@ -44,20 +45,29 @@ from sed_fit.stellar_grids import StellarGrid
 # Use a non-interactive matplotlib backend to avoid threading errors.
 mpl_use("agg")
 
-subs = ["A", "B"] # Fixed at 2 stars
-theta_plot_captions = np.array([f"$T_{{\\rm eff,{sub}}} / {{\\rm K}}$" for sub in subs] \
-                            +[f"$\\log{{g}}_{{\\rm {sub}}}$" for sub in subs] \
-                            +[f"$R_{{\\rm {sub}}} / {{\\rm R_{{\\odot}}}}$" for sub in subs] \
-                            +["${\\rm dist} / {\\rm pc}$", "${\\rm A_{V}}$"])
-theta_captions = np.array([(f"Teff{sub}", u.K) for sub in subs] \
-                        +[(f"logg{sub}", u.dex) for sub in subs] \
-                        +[(f"R{sub}", u.Rsun) for sub in subs] \
-                        +[("dist", u.pc), ("av", u.dimensionless_unscaled)])
-result_columns = [t for t, _ in theta_captions]
-label_columns = result_columns + [f"logL{sub}" for sub in subs]
+def subs(num_stars: int=2):
+    return ("ABCDEFGHIJKLM"[n] for n in range(num_stars))
+
+def theta_plot_captions(num_stars: int=2) -> np.ndarray[str]:
+    return np.array([f"$T_{{\\rm eff,{sub}}} / {{\\rm K}}$" for sub in subs(nstars)] \
+                    + [f"$\\log{{g}}_{{\\rm {sub}}}$" for sub in subs(nstars)] \
+                    + [f"$R_{{\\rm {sub}}} / {{\\rm R_{{\\odot}}}}$" for sub in subs(nstars)] \
+                    + ["${\\rm dist} / {\\rm pc}$", "${\\rm A_{V}}$"])
+
+def theta_captions(num_stars: int=2) -> np.ndarray[tuple[str, u.UnitBase]]:
+    return np.array([(f"Teff{sub}", u.K) for sub in subs(nstars)] \
+                    + [(f"logg{sub}", u.dex) for sub in subs(nstars)] \
+                    + [(f"R{sub}", u.Rsun) for sub in subs(nstars)] \
+                    + [("dist", u.pc), ("av", u.dimensionless_unscaled)])
+
+def result_colnames(num_stars: int=2, label_cols: bool=False) -> List[str]:
+    cols = [t for t, _ in theta_captions(num_stars)]
+    if label_cols:
+        return cols + [f"logL{sub}" for sub in subs(nstars)]
+    return cols
 
 def print_fitted_params(theta: np.ndarray, fitted_mask: np.ndarray,
-                        known_values_dict: dict, labels: np.ndarray=theta_captions):
+                        known_values_dict: dict, labels: np.ndarray):
     """ Pretty printer for fitted params in rows with known values in brackets."""
     for (param, unit), val, fitted in zip(labels, theta, fitted_mask):
         kval = None
@@ -85,14 +95,6 @@ if __name__ == "__main__":
                     use_av_override=False)
     args = ap.parse_args()
     run_details = f"{datetime.now():%Y-%m-%d %H:%M:%S%z %Z} $ {' '.join(orig_argv)}"
-
-    # Summarise how the priors are handled for each theta item. Each flag controls whether the
-    # corresponding value is fixed at known val with no prior (0), constrained by a Gaussian prior
-    # for a known "truth" value, or fully free and constrained by Gaussian prior on a ratio (2).
-    #                for:  Teffs   loggs   radii   dist  Av
-    prior_flags = np.array([2]*2 + [1]*2 + [2]*2 + [1] + [1], dtype=int)
-    # These slices are useful shortcut to subset of fit|prior flags and theta.
-    theta_slices = np.array([slice(0, 2), slice(2, 4), slice(4, 6), slice(6, 7), slice(7, 8)])
 
     # Get the targets' configurations
     with open(args.targets_file, mode="r", encoding="utf8") as f:
@@ -126,14 +128,24 @@ if __name__ == "__main__":
         print(f"Command: {' '.join(orig_argv)}")
         print(f"Directory for data, logs & plots: {out_dir}\n", flush=True)
 
+        # Summarise how the priors are handled for each theta item. Each flag controls whether the
+        # corresponding value is fixed at known val & no prior (0), constrained by a Gaussian prior
+        # for a known "truth" value, or fully free and constrained by Gaussian prior on a ratio (2).
+        #                for:  Teffs        loggs        radii        dist  Av
+        nstars = 2 # TODO: can only support 2 stars until ratio handling updated
+        prior_flags = np.array([2]*nstars + [1]*nstars + [2]*nstars + [1] + [1], dtype=int)
+        # These slices are useful shortcut to subset of fit|prior flags and theta.
+        theta_slices = np.array([slice(0,nstars), slice(nstars,2*nstars), slice(2*nstars,3*nstars),
+                                 slice(3*nstars,3*nstars+1), slice(3*nstars+1,3*nstars+2)])
+
         # Set up the CSV files that will hold the results
-        for csv, cols, hd_fmt in [(lbl_csv, label_columns, r"{0},{0}_err"),
-                                  (fit_csv, result_columns, r"{0}"),
-                                  (mcmc_csv,  result_columns, r"{0},{0}_err")]:
+        for csv, cols, hfmt in [(lbl_csv, result_colnames(nstars, label_cols=True), r"{0},{0}_err"),
+                                (fit_csv, result_colnames(nstars), r"{0}"),
+                                (mcmc_csv,  result_colnames(nstars), r"{0},{0}_err")]:
             if not "mcmc" in csv.name or not args.mcmc_off:
                 with open(csv, mode="a", encoding="utf8") as f:
                     # OK to append headers as they're comments and should be ignored if in the body
-                    f.write("# target," + ",".join(hd_fmt.format(c) for c in cols) + "\n")
+                    f.write("# target," + ",".join(hfmt.format(c) for c in cols) + "\n")
                     f.write("# " + run_details + "\n")
 
         # Extinction model: G23 (Gordon et al., 2023) Milky Way R(V) filter gives broadest coverage
@@ -154,10 +166,10 @@ if __name__ == "__main__":
                 f"with fluxes returned in units of {stellar_grid.flux_unit:unicode}")
 
         # Fixed limit priors
-        teff_limits = stellar_grid.teff_range
-        logg_limits = stellar_grid.logg_range
-        radius_limits = (0.1, 100)
-        av_limits = (-1, 5)
+        teff_lims = stellar_grid.teff_range
+        logg_lims = stellar_grid.logg_range
+        rad_lims = (0.1, 100)
+        av_lims = (-1, 5)
 
         for tix, (target, config) in enumerate(targets_cfg.items(), start=1):
             try:
@@ -167,26 +179,24 @@ if __name__ == "__main__":
                 plots_dir = figs_dir / to_file_safe_str(target)
                 plots_dir.mkdir(parents=True, exist_ok=True)
 
-
-
                 # Create any missing config values
                 config.setdefault("search_term", target)
                 config.setdefault("Teff_sys",nom_val(estimate_teff_from_spt(config.get("SpT","F"))))
                 config.setdefault("logg_sys", 4.0)
-                for sub in subs:
+                for sub in subs(nstars):
                     if f"logg{sub}" not in config:
                         logg = log_g(
                             ufloat(config[f"M{sub}"], config.get(f"M{sub}_err", 0)) * M_sun.value,
                             ufloat(config[f"R{sub}"], config.get(f"R{sub}_err", 0)) * R_sun.value)
                         config[f"logg{sub}"], config[f"logg{sub}_err"] = nom_val(logg),std_dev(logg)
-                for k in ["Teff", "logg"]:
-                    if f"{k}R" not in config:
-                        nomA, nomB = config[f"{k}A"], config[f"{k}B"]
-                        ratio = ufloat(nomB, config.get(f"{k}B_err", None) or nomB * 0.05) \
-                                / ufloat(nomA, config.get(f"{k}A_err", None) or nomA * 0.05)
-                        config[f"{k}R"], config[f"{k}R_err"] = nom_val(ratio), std_dev(ratio)
+                for c in ["Teff", "logg"]: # TODO: handle multiple ratios for >2 stars
+                    if f"{c}R" not in config:
+                        nomA, nomB = config[f"{c}A"], config[f"{c}B"]
+                        ratio = ufloat(nomB, config.get(f"{c}B_err", None) or nomB * 0.05) \
+                                / ufloat(nomA, config.get(f"{c}A_err", None) or nomA * 0.05)
+                        config[f"{c}R"], config[f"{c}R_err"] = nom_val(ratio), std_dev(ratio)
 
-                if not all(k in config for k in ["ruwe", "ra", "dec", "parallax"]):
+                if not all(c in config for c in ["ruwe", "ra", "dec", "parallax"]):
                     print(f"Querying Gaia DR3 for coordinates and ruwe of {target}")
                     dr3_id = int(config["gaia_dr3_id"])
                     if _tbl := Gaia.launch_job("SELECT TOP 1 * FROM gaiadr3.gaia_source_lite " \
@@ -233,11 +243,11 @@ if __name__ == "__main__":
                 # Set up the priors and the ln_prior_func.
                 # Always setup both ratios & values, reading them from config.
                 ratio_priors = []
-                for k in ["TeffR", "loggR", "k"]:
-                    ratio_priors += [None, ufloat(config[k], config[f"{k}_err"])]
+                for c in ["TeffR", "loggR", "k"]: # TODO: handle multiple ratios for >2 stars
+                    ratio_priors += [None, ufloat(config[c], config[f"{c}_err"])]
 
-                value_priors = [ufloat(config[f"{k}{sub}"], config.get(f"{k}{sub}_err", None) or 0)
-                                                    for k in ["Teff", "logg", "R"] for sub in subs]
+                value_priors = [ufloat(config[f"{n}{sub}"], config.get(f"{n}{sub}_err", None) or 0)
+                                            for n in ["Teff", "logg", "R"] for sub in subs(nstars)]
                 value_priors += [dist]
                 if "av_override" in config and args.use_av_override:
                     value_priors += [ufloat(config["av_override"], 0.05)]
@@ -258,33 +268,34 @@ if __name__ == "__main__":
                     if not std_dev(vp):
                         value_priors[ix] = ufloat(nom_val(vp), nom_val(vp) * 0.05)
 
-                # Print out the chosen prior values
+                # Print out the chosen prior values. TODO: handle multiple ratios for >2 stars
                 msg = ""
-                for k, sl in zip(["Teff", "logg", "rad", "dist", "av"], theta_slices):
+                for c, sl in zip(["Teff", "logg", "rad", "dist", "av"], theta_slices):
                     if all(prior_flags[sl] == 2):
-                        msg += f"{k}R={ratio_priors[sl.stop-1]:.3f}, "
+                        msg += f"{c}R={ratio_priors[sl.stop-1]:.3f}, "
                     elif all(prior_flags[sl] == 1):
                         if sl.stop - sl.start == 1:
-                            msg += f"{k}={value_priors[sl.start]:.3f}, "
+                            msg += f"{c}={value_priors[sl.start]:.3f}, "
                         else:
-                            for i, vp in enumerate(value_priors[sl]):
-                                msg += f"{k}{i+1}={vp:.3f}, "
+                            for vp, sub in zip(value_priors[sl], subs(nstars)):
+                                msg += f"{c}{sub}={vp:.3f}, "
                 print("\nPriors:", msg.rstrip(", "))
 
                 def ln_prior_func(theta: np.ndarray[float]) -> float:
                     """ fitting prior callback function to evaluate the current candidate theta """
                     # pylint: disable=cell-var-from-loop
-                    if not all(teff_limits[0] <= t <= teff_limits[1] for t in theta[0:2]) \
-                        or not all(logg_limits[0] <= l <= logg_limits[1] for l in theta[2:4]) \
-                        or not all(radius_limits[0] <= r <= radius_limits[1] for r in theta[4:6]) \
+                    if not all(teff_lims[0]<= t <=teff_lims[1] for t in theta[theta_slices[0]]) \
+                        or not all(logg_lims[0]<= l <=logg_lims[1] for l in theta[theta_slices[1]])\
+                        or not all(rad_lims[0]<= r <=rad_lims[1] for r in theta[theta_slices[2]]) \
                         or not 0 < theta[-2] \
-                        or not av_limits[0] <= theta[-1] <= av_limits[1]:
+                        or not av_lims[0] <= theta[-1] <= av_lims[1]:
                         return -np.inf
 
                     # prior_flags:  0|False = fixed so no prior
                     #               1|True  = direct Gaussian prior on a "truth" value +/- sigma
                     #               2       = Gaussian prior on a ratio value +/- sigma
                     # Use 1 for "truths" with uncertainties and 2 where fitting to find values
+                    # TODO: handle multiple ratios for >2 stars
                     ret_val = 0
                     for ti, prior_flag in enumerate(prior_flags):
                         if prior_flag == 1:
@@ -299,17 +310,18 @@ if __name__ == "__main__":
 
                 # Save the labels to a csv.
                 with lbl_csv.open("a", encoding="utf8") as f:
-                    f.write(f"{target}," + ",".join(f"{config[c]:.6e},{config.get(f'{c}_err', None) or 0:.6e}" for c in label_columns) +"\n") # pylint: disable=line-too-long
+                    f.write(f"{target}," + ",".join(f"{config[c]:.6e},{config.get(f'{c}_err', None) or 0:.6e}" for c in result_colnames(nstars, True)) +"\n") # pylint: disable=line-too-long
 
 
 
                 # Initial Teffs, loggs & radii. If free fit, we use sys val modified by ratio priors
+                # TODO: handle multiple ratios for >2 stars
                 fit_mask = prior_flags > 0
                 theta0 = nom_vals(value_priors)
-                for k, sl in zip(["Teff", "logg", "R"], theta_slices[:3]):
+                for c, sl in zip(["Teff", "logg", "R"], theta_slices[:3]):
                     if all(prior_flags[sl] == 2):
                         ratio = nom_val(ratio_priors[sl.stop-1])
-                        t0 = t0 = config["Teff_sys"] / 5500 if k == "R" else config[f"{k}_sys"]
+                        t0 = t0 = config["Teff_sys"] / 5500 if c == "R" else config[f"{c}_sys"]
                         theta0[sl] = (t0, t0*ratio) if ratio < 1 else (t0/ratio, t0)
 
 
@@ -331,7 +343,7 @@ if __name__ == "__main__":
                                             ln_prior_func, methods=["Nelder-Mead"], verbose=True)
                 print("Fitted params from minimize fit. Those marked * were fitted.",
                     "Known/published values are in brackets.")
-                print_fitted_params(theta_fit, fit_mask, known_values_dict=config)
+                print_fitted_params(theta_fit, fit_mask, config, theta_captions(nstars))
 
                 fig = plot_fitted_model(sed, theta_fit, stellar_grid, sed_flux_colname="sed_flux",
                                         show_combined_spectrum=True, show_component_spectra=False,
@@ -360,8 +372,8 @@ if __name__ == "__main__":
                                                progress=True, verbose=True)
 
                 print("Fitted params from MCMC sampling. Those marked * were fitted.",
-                    "Known/published values are in brackets.")
-                print_fitted_params(theta_mcmc, fit_mask, known_values_dict=config)
+                      "Known/published values are in brackets.")
+                print_fitted_params(theta_mcmc, fit_mask, config, theta_captions(nstars))
 
                 fig = plot_fitted_model(sed, theta_mcmc, stellar_grid, sed_flux_colname="sed_flux",
                                         show_combined_spectrum=True, show_component_spectra=False,
@@ -372,7 +384,7 @@ if __name__ == "__main__":
                 samples = samples_from_sampler(sampler, flat=True)
                 fig = corner.corner(samples, show_titles=True, plot_datapoints=True,
                                     quantiles=[0.16, 0.5, 0.84],
-                                    labels=theta_plot_captions[fit_mask],
+                                    labels=theta_plot_captions(nstars)[fit_mask],
                                     truths=nom_vals(theta_mcmc[fit_mask]))
                 fig.savefig(plots_dir / "sed-mcmc-corner.pdf")
                 plt.close(fig)
