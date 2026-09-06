d8e2a27f3da3c5bdbfe323c068c7234351a09b16
SteveOv <steveoverall@hotmail.co.uk>
2026-09-06 12:29:29 +0100

    now prints means of Kervella distances, mcmc fit and known values

--- fit_testing_distances.py
+++ fit_testing_distances.py
@@ -12,7 +12,9 @@ import numpy as np
 # pylint: disable=wrong-import-position
 warnings.filterwarnings("ignore", "Using UFloat objects with std_dev==0 may give unexpected results.", category=UserWarning) # pylint: disable=line-too-long
 from uncertainties import ufloat
+from uncertainties.unumpy import nominal_values as nom_vals, std_devs
 from astroquery.simbad import Simbad
+from astropy.units import Rsun
 from deblib.vmath import wrap_func_for_uncertainties
 
 from fit_testing_results import read_result_csv
@@ -68,8 +70,8 @@ if __name__ == "__main__":
             res_row = mcmc_vals[mcmc_vals["target"] == sterm_index[sterm]][0]
             ebv = res_row["av"] / 3.1
             TeffA, TeffB, RA, RB = res_row[["TeffA", "TeffB", "RA", "RB"]]
-            print(f"\n{sterm_index[sterm]} has a MCMC derived distance of {res_row['dist']:.6f} pc",
-                  f"and E(B-V) of {ebv:.6f} mag (where RV=3.1)")
+            print(f"\n{sterm_index[sterm]} MCMC yields TeffA={TeffA:.3uf} & TeffB={TeffB:.3uf} K,",
+                  f"RA={RA:.3uf} & RB={RB:.3uf} {Rsun:unicode} and E(B-V)={ebv:.3uf} mag (RV=3.1).")
 
             print("The distances calculated with the Kervella+ (2004A&A...426..297K)",
                   "surface brightness-Teff relation are:")
@@ -78,6 +80,7 @@ if __name__ == "__main__":
             flux_mask &= tbl["flux.qual"] == "C"
             # flux_mask &= np.isfinite(tbl["flux_err"])
 
+            dists_for_mean = {}
             for flux_row in tbl[flux_mask]:
                 band = flux_row["flux.filter"]
                 mag = ufloat(flux_row["flux"], flux_row["flux_err"] or 0)
@@ -87,3 +90,37 @@ if __name__ == "__main__":
                 print("\t( err contribs:",
                     ", ".join(f"{v.tag}={e/dist.s:.3f}" for v,e in dist.error_components().items()),
                     ")")
+                if band in ("J", "H", "K"):
+                    dists_for_mean[band] = dist
+
+            if len(dists_for_mean) > 0:
+                # Arithmetic mean
+                mean_bands, dists = tuple(dists_for_mean.keys()), tuple(dists_for_mean.values())
+                print(f"Arithmetic mean distance {mean_bands}:        {np.mean(dists):.6f} pc")
+
+                # Unbiased weighted sample mean
+                # The (reliability) weights: w_i = 1 / sig_i^2 and W = SUM(w_i) and V = SUM(w_i^2)
+                # The **weighted sample mean**: xbar_w = 1/W * SUM(w_i * x_i)
+                # The biased weighted sample variance: sig2_w = 1/W * SUM(w_i * (x_i - xbar_w)^2)
+                # The **unbiased weighted sample variance**: s2_w = sig2_w / (1 - (V / W^2))
+                x_i, sig_i = nom_vals(dists), std_devs(dists)
+                sig_i[sig_i == 0] = 1e-21
+                w_i = np.reciprocal(np.square(sig_i))
+                big_w, big_v = np.sum(w_i), np.sum(np.square(w_i))
+
+                xbar_w = np.divide(np.sum(np.multiply(w_i, x_i)), big_w)
+                sig2_w = np.divide(np.sum(np.multiply(w_i, np.power(x_i - xbar_w, 2))), big_w)
+                s2_w = np.divide(sig2_w, np.subtract(1, np.divide(big_v, np.square(big_w))))
+                unbiased_samp_mean = ufloat(xbar_w, np.sqrt(s2_w))
+                print(f"Unbiased weighted mean distance {mean_bands}: {unbiased_samp_mean:.6f} pc")
+
+            kndist, knbib = None, None
+            if "dist" in config:
+                kndist = ufloat(config["dist"], config.get("dist_err", 0)) # pc
+                knbib = config.get("dist_bibcode", "")
+            elif "parallax" in config:
+                kndist = 1000 / ufloat(config["parallax"], config.get("parallax_err", 0)) # mas
+                knbib = config.get("parallax_bibcode", "")
+            if kndist:
+                print(f"                                 Known distance: {kndist:.6f} pc ({knbib})")
+            print(f"{sterm_index[sterm]:>28s} distance from MCMC: {res_row['dist']:.6f} pc")
